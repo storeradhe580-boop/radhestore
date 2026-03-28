4ab2623cdc2234aa1876b649972e49b2adc2bcec
developer <developer0800@gmail.com>
2026-03-28 13:45:56 +0530

    Fix: Remove SoftDeletes from Category model - table doesn't have deleted_at column

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,12 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
-    use SoftDeletes;
-    
     protected $fillable = ['name', 'slug', 'image', 'status'];
     
     protected $casts = [
